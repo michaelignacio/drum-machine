2d323d184d2ca4a1297bf35f607a08de3f651eb6
Michael Ignacio <mrignacio1@up.edu.ph>
2018-06-14 19:37:19 +0800

    Refactored code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import './App.css';
+import DrumPad from './DrumPad.js';
 import data from './data.js';
+import './App.css';
 
 class App extends Component {
   constructor(props) {
@@ -35,59 +36,10 @@ class App extends Component {
           <div id="display">
             Now Playing: {this.state.nowPlaying}
           </div>
-          {listItems}
+          <div className="drum-pad-area">
+            {listItems}
+          </div>
         </div>
-      </div>
-    );
-  }
-}
-
-class DrumPad extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-
-    this.handleKeyPress = this.handleKeyPress.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  componentDidMount() {
-    document.addEventListener('keydown', this.handleKeyPress);
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.handleKeyPress);
-  }
-
-  handleClick() {
-    let audio = document.getElementById(this.props.text);
-    audio.play();
-    this.props.onInstrumentChange(this.props.id);
-  }
-
-  handleKeyPress(event) {
-    if (event.key === this.props.text.toLowerCase()) {
-      let audio = document.getElementById(this.props.text);
-      audio.play();
-      this.props.onInstrumentChange(this.props.id);
-    }
-  }
-
-  render() {
-    return (
-      <div 
-        className="drum-pad" 
-        id={this.props.id} 
-        onClick={this.handleClick}
-        onKeyPress={this.handleKeyPress}
-      >
-        {this.props.text}
-        <audio 
-          src={this.props.src} 
-          id={this.props.text}
-          className="clip"
-        >
-        </audio>
       </div>
     );
   }
