8fd2e1e184ae82e7e83c89f013105ff5c339be2e
Michael Ignacio <mrignacio1@up.edu.ph>
2018-06-14 18:54:57 +0800

    Added array of objects for storage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,54 @@
 import React, { Component } from 'react';
 import './App.css';
 
+let data = [
+{  
+  id: "heater-1",
+  text: "Q",
+  src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+},
+{
+  id: "heater-2",
+  text: "W",
+  src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
+},
+{
+  id: "heater-3",
+  text: "E",
+  src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
+},
+{  
+  id: "heater-4",
+  text: "A",
+  src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
+},
+{ 
+  id: "clap",
+  text: "S",
+  src: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3"
+},
+{
+  id: "open-hh",
+  text: "D",
+  src: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3"
+},
+{ 
+  id: "kick-n-hat",
+  text: "Z",
+  src: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
+},
+{
+  id: "kick",
+  text: "X",
+  src: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
+},
+{
+  id: "closed-hh",
+  text: "C",
+  src: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+}
+];
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -15,68 +63,26 @@ class App extends Component {
   }
 
   render() {
+    const listItems = data.map(item => {
+      return (
+        <DrumPad
+          key={item.id}
+          id={item.id}
+          text={item.text}
+          src={item.src}
+          onInstrumentChange={this.handleInstrumentChange}
+          >
+        </DrumPad>
+      );
+    });
+
     return (
       <div className="App">
         <div id="drum-machine">
           <div id="display">
             Now Playing: {this.state.nowPlaying}
           </div>
-
-          {/*Optimize this part*/}
-          <DrumPad
-            id="heater-1"
-            text="Q"
-            src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
-            onInstrumentChange={this.handleInstrumentChange}
-          />
-          <DrumPad
-            id="heater-2"
-            text="W"
-            onInstrumentChange={this.handleInstrumentChange}
-            src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
-          />
-          <DrumPad
-            id="heater-3"
-            text="E"
-            onInstrumentChange={this.handleInstrumentChange}
-            src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
-          />
-          <DrumPad
-            id="heater-4"
-            text="A"
-            onInstrumentChange={this.handleInstrumentChange}
-            src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
-          />
-          <DrumPad
-            id="clap"
-            text="S"
-            onInstrumentChange={this.handleInstrumentChange}
-            src="https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3"
-          />
-          <DrumPad
-            id="open-hh"
-            text="D"
-            onInstrumentChange={this.handleInstrumentChange}
-            src="https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3"
-          />
-          <DrumPad
-            id="kick-n-hat"
-            text="Z"
-            onInstrumentChange={this.handleInstrumentChange}
-            src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
-          />
-          <DrumPad
-            id="kick"
-            text="X"
-            onInstrumentChange={this.handleInstrumentChange}
-            src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
-          />
-          <DrumPad
-            id="closed-hh"
-            text="C"
-            onInstrumentChange={this.handleInstrumentChange}
-            src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
-          />
+          {listItems}
         </div>
       </div>
     );
