e0175789ac72b1b08e61cf7df0a7a053adb3d1dd
Michael Ignacio <mrignacio1@up.edu.ph>
2018-06-14 19:06:49 +0800

    Moved array to separate file

--- src/App.js
+++ src/App.js
@@ -1,53 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-
-let data = [
-{  
-  id: "heater-1",
-  text: "Q",
-  src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
-},
-{
-  id: "heater-2",
-  text: "W",
-  src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
-},
-{
-  id: "heater-3",
-  text: "E",
-  src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
-},
-{  
-  id: "heater-4",
-  text: "A",
-  src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
-},
-{ 
-  id: "clap",
-  text: "S",
-  src: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3"
-},
-{
-  id: "open-hh",
-  text: "D",
-  src: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3"
-},
-{ 
-  id: "kick-n-hat",
-  text: "Z",
-  src: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
-},
-{
-  id: "kick",
-  text: "X",
-  src: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
-},
-{
-  id: "closed-hh",
-  text: "C",
-  src: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
-}
-];
+import data from './data.js';
 
 class App extends Component {
   constructor(props) {
@@ -63,7 +16,7 @@ class App extends Component {
   }
 
   render() {
-    const listItems = data.map(item => {
+    const listItems = data.content.map(item => {
       return (
         <DrumPad
           key={item.id}
---
+++ src/data.js
@@ -0,0 +1,49 @@
+export default {
+  content: [
+    {  
+      id: "heater-1",
+      text: "Q",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+    },
+    {
+      id: "heater-2",
+      text: "W",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
+    },
+    {
+      id: "heater-3",
+      text: "E",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
+    },
+    {  
+      id: "heater-4",
+      text: "A",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
+    },
+    { 
+      id: "clap",
+      text: "S",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3"
+    },
+    {
+      id: "open-hh",
+      text: "D",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3"
+    },
+    { 
+      id: "kick-n-hat",
+      text: "Z",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
+    },
+    {
+      id: "kick",
+      text: "X",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
+    },
+    {
+      id: "closed-hh",
+      text: "C",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+    }
+  ]
+};
